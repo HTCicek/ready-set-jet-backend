74658d116b33cfb6af5f3664abeeee811c0b9bf2
H. Turcan Cicek <htcicek@users.noreply.github.com>
2019-12-10 17:06:38 -0500

    renamed Flight attribute to be more clear

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,5 +1,5 @@
 class Flight < ApplicationRecord
   belongs_to :user
-  has_one :arrival
-  has_one :departure
+  has_one :destination, class_name: 'Arrival'
+  has_one :origin, class_name: 'Departure'
 end
